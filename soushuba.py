158c2f1e239d622c82d8fd0ac401357a06cb0280
shiyunqi001 <73279178+shiyunqi001@users.noreply.github.com>
2023-09-12 09:56:12 +0800

    Update soushuba.py

--- soushuba.py
+++ soushuba.py
@@ -21,18 +21,13 @@ ch.setLevel(logging.INFO)
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 def sendmsg(msg):
-    data = {
-        "qq": "1351714146",
-        "msg": msg,
-    }
     headers = {
-            "Connection": "keep-alive",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
             "Accept-Language": "zh-CN,cn;q=0.9",
             "Content-Type": "application/x-www-form-urlencoded",
         }
-    requests.post('http://home.undem.cn:6789/send/bf89f9759a9478d2979fd6ab13152fc6',data=data,headers=headers)
+    requests.get('http://home.undem.cn:6789/send/bf89f9759a9478d2979fd6ab13152fc6?&qq=1351714146&msg='+msg,headers=headers)
 def geturl(x):
     a = requests.get(x)
     if (len(a.text)<20000):
